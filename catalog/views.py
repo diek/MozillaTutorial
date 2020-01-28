31366fde33a6b600bae8e4f9f519ae2881ffed2c
diek <d_kearney@bellaliant.net>
2020-01-27 22:59:56 -0300

    Add initial permissions

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from catalog.models import Author, Book, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 from locallibrary.settings import SITE_AUTHOR
@@ -41,3 +42,13 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
