b38722ebd9105d157d78ae264959d5ea525d8e5f
diek <d_kearney@bellaliant.net>
2020-01-27 22:28:29 -0300

    Update admin to include 'borrower' in bookinstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -16,6 +16,7 @@ admin.site.register(Language)
 
 class BooksInline(admin.TabularInline):
     """Defines format of inline book insertion (used in AuthorAdmin)"""
+
     model = Book
 
 
@@ -28,14 +29,15 @@ class AuthorAdmin(admin.ModelAdmin):
        grouping the date fields horizontally
      - adds inline addition of books in author view (inlines)
     """
-    list_display = ('last_name',
-                    'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
     inlines = [BooksInline]
 
 
 class BooksInstanceInline(admin.TabularInline):
     """Defines format of inline book instance insertion (used in BookAdmin)"""
+
     model = BookInstance
 
 
@@ -45,7 +47,8 @@ class BookAdmin(admin.ModelAdmin):
      - fields to be displayed in list view (list_display)
      - adds inline addition of book instances in book view (inlines)
     """
-    list_display = ('title', 'author', 'display_genre')
+
+    list_display = ("title", "author", "display_genre")
     inlines = [BooksInstanceInline]
 
 
@@ -60,16 +63,12 @@ class BookInstanceAdmin(admin.ModelAdmin):
      - filters that will be displayed in sidebar (list_filter)
      - grouping of fields into sections (fieldsets)
     """
+
     # list_display = ('book', 'status', 'borrower', 'due_back', 'id')
-    list_display = ('book', 'status', 'due_back', 'id')
-    list_filter = ('status', 'due_back')
+    list_display = ("book", "status", "borrower", "due_back", "id")
+    list_filter = ("status", "due_back")
 
     fieldsets = (
-        (None, {
-            'fields': ('book', 'imprint', 'id')
-        }),
-        ('Availability', {
-            # 'fields': ('status', 'due_back', 'borrower')
-            'fields': ('status', 'due_back')
-        }),
+        (None, {"fields": ("book", "imprint", "id")}),
+        ("Availability", {"fields": ("status", "due_back", "borrower")}),
     )
