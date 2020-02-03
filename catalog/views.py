feedde908141c674284f1029671e98553d562dba
diek <d_kearney@bellaliant.net>
2020-02-03 14:44:26 -0300

    Fix padding in side nav, code compression of views

--- catalog/views.py
+++ catalog/views.py
@@ -45,10 +45,10 @@ class BookDetailView(generic.DetailView):
     paginate_by = 20
 
 
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
-    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
     paginate_by = 10
 
     def get_queryset(self):
